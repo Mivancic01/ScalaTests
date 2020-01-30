2fe2d72653d97ac62ae6a84abea703bbeaab66ed
Mivancic01 <m.ivancic@student.tugraz.at>
2020-01-30 10:29:41 +0100

    Initial project

---
+++ play-samples-play-scala-hello-world-tutorial/target/scala-2.13/routes/main/router/RoutesPrefix.scala
@@ -0,0 +1,15 @@
+// @GENERATOR:play-routes-compiler
+// @SOURCE:D:/Directories/EssentialPlay Test Programs/ScalaTests/play-samples-play-scala-hello-world-tutorial/conf/routes
+// @DATE:Thu Jan 30 10:20:56 CET 2020
+
+
+package router {
+  object RoutesPrefix {
+    private var _prefix: String = "/"
+    def setPrefix(p: String): Unit = {
+      _prefix = p
+    }
+    def prefix: String = _prefix
+    val byNamePrefix: Function0[String] = { () => prefix }
+  }
+}
